1d33bc704707a0fdc83d3b1e3c5ed540257b9284
Ye Thiha <mightycryptowall@gmail.com>
2022-01-15 13:09:22 +0630

    hover -e -a

--- src/components/MultiSlider.js
+++ src/components/MultiSlider.js
@@ -6,30 +6,39 @@ const items = [
   {
     key: 1,
     bgColor: "red",
+    bgImage: "http://localhost:1337/uploads/deadpool_3_4bc27ce246.png",
   },
   {
     key: 2,
     bgColor: "blue",
+    bgImage:
+      "http://localhost:1337/uploads/deadpool_vs_wolverine_6f3a5f148d.png",
   },
   {
     key: 3,
     bgColor: "green",
+    bgImage: "http://localhost:1337/uploads/fantastic_four_da534274f1.png",
   },
   {
     key: 4,
     bgColor: "yellow",
+    bgImage: "http://localhost:1337/uploads/real_steel_2_79beefa164.png",
   },
   {
     key: 5,
     bgColor: "indigo",
+    bgImage:
+      "http://localhost:1337/uploads/deadpool_vs_wolverine_6f3a5f148d.png",
   },
   {
     key: 6,
     bgColor: "crimson",
+    bgImage: "http://localhost:1337/uploads/fantastic_four_da534274f1.png",
   },
   {
     key: 7,
     bgColor: "lime",
+    bgImage: "http://localhost:1337/uploads/real_steel_2_79beefa164.png",
   },
 ];
 
@@ -42,11 +51,9 @@ const MultiSlider = () => {
   const [listTransform, setListTransform] = useState("translateX(0px)");
 
   const calListItemWidth = () => {
-    
-
-     if (size.width < 640) return 100;
-     if (size.width < 1007) return 200;
-     return 300
+    if (size.width < 640) return 100;
+    if (size.width < 1007) return 200;
+    return 300;
   };
 
   const productListItemWidth = calListItemWidth();
@@ -81,13 +88,12 @@ const MultiSlider = () => {
   function moveProductList(value) {
     setListTransform(`translateX(-${productListItemWidth * value}px)`);
   }
-  
 
   return (
     <div className="carousel js-product-carousel">
       <div
         className="carousel-view"
-        style={{ width: productListItemWidth * 4 }}
+        style={{ width: productListItemWidth * 4, height: (productListItemWidth * (1/1.5))}}
       >
         <span
           className="carousel-control js-carousel-prev"
@@ -110,11 +116,19 @@ const MultiSlider = () => {
               className="product-list-item"
               key={item.key}
               style={{
-                backgroundColor: item.bgColor,
+                backgroundImage: `url(${item.bgImage})`,
+                backgroundSize: "100%",
                 width: productListItemWidth,
+                height: (productListItemWidth * (1/1.5)),
+                backgroundRepeat: "no-repeat",
               }}
             >
-              <div data-slide="1" className="product"></div>
+              <div data-slide="1" className="product thumbnail">
+                <div className="thumbnail-text">
+                  <h5>Better Together Better Myanmar</h5>
+                  <p>with Mosaic Myanmar</p>
+                </div>
+              </div>
             </li>
           ))}
         </ul>
